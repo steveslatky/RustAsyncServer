11d3be4c468d43769500b06d379e29ff020bcca7
Stephen Slatky <steve.slatky@gmail.com>
2017-08-30 10:00:37 -0400

    Starting tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate bytes;
+extern crate futures;
+extern crate tokio_io;
+extern crate tokio_proto;
+extern crate tokio_service;
+
+use std::io;
+use std::str;
+use bytes::BytesMut;
+use tokio_io::codec::{Encoder, Decoder};
+
+pub struct LineCodec;
+
+impl Decoder for LineCodec {
+    type Item = String;
+    type Error = io::Error;
+
+    // ...
+}
+
+impl Encoder for LineCodec {
+    type Item = String;
+    type Error = io::Error;
+
+    // ...
+}
+
 fn main() {
     println!("Hello, world!");
 }
+
